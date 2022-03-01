a2a2eb577d3891c184336845853cfbcb15f7d0a2
interma <interma@outlook.com>
2022-03-01 09:27:28 +0800

    fix reviewer's comment

--- src/backend/cdb/motion/ic_tcp.c
+++ src/backend/cdb/motion/ic_tcp.c
@@ -2231,8 +2231,7 @@ format_sockaddr(struct sockaddr *sa, char *buf, int bufsize)
 #ifdef HAVE_IPV6
 	else if (sa->sa_family == AF_INET6)
 	{
-		char		remote_port[32];
-		remote_port[0] = '\0';
+		char		remote_port[32] = {'\0'};
 
 		if (bufsize > 10)
 		{
